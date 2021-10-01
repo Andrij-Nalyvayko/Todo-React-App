f5523b713e9965e6137d507755e8902ee556110d
Andrij Nalyvayko <andrij.nalyvayko@gmail.com>
2021-10-01 12:42:36 +0300

    Updates

---
+++ precache-manifest.a91d6026076566ce6ce22bd79ccd47d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "645bb758ddf67701b6bb2abc46491f2e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "be23c45002e4aa8de733",
+    "url": "./static/css/main.0f4408bb.chunk.css"
+  },
+  {
+    "revision": "3c767f392da664d412bd",
+    "url": "./static/js/2.c20f06e1.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "./static/js/2.c20f06e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be23c45002e4aa8de733",
+    "url": "./static/js/main.2132994d.chunk.js"
+  },
+  {
+    "revision": "35878c0637cea8221062",
+    "url": "./static/js/runtime-main.ba67aef9.js"
+  }
+]);
